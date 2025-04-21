c8ccb71150310d2e1d11e15dbe130e2169b2b1a8
Sarthak-Vatsa <156640974+Sarthak-Vatsa@users.noreply.github.com>
2025-04-22 00:32:33 +0530

    Integrated OAuth for both consumers and lawyers

--- LegalEaseSB/src/main/java/com/legalease/LegalEaseSB/Env/TestEnv.java
+++ LegalEaseSB/src/main/java/com/legalease/LegalEaseSB/Env/TestEnv.java
@@ -10,8 +10,16 @@ public class TestEnv {
     @Value("${MONGO_URI}")
     private String mongoUri;
 
+    @Value("${GOOGLE_CLIENT_ID}")
+    private String googleClientId;
+
+    @Value("${GOOGLE_CLIENT_SECRET}")
+    private String googleClientSecret;
+
     @PostConstruct
     public void printEnv() {
+        System.out.println("GOOGLE_CLIENT_ID: " + googleClientId);
+        System.out.println("GOOGLE_CLIENT_SECRET: " + googleClientSecret);
         System.out.println("Mongo URI from env: " + mongoUri);
     }
 }
